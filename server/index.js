e4d7cc36d2d4d9fb422e5b27f750d586b1ab2a66
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-06-16 15:39:05 +0300

    backend in progress

--- server/index.js
+++ server/index.js
@@ -4,18 +4,25 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5002
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'WORKING'})
 // })
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const userController = require('../controllers/userController')
+const autMiddleware = require('../middleware/authMiddleware')
 
 const router = new Router()
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', autMiddleware, userController.check)
 
 module.exports = router
