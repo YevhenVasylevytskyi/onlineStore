9f368300374e938125a3b5d7112de2b418a9b8fc
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-07-23 13:00:17 +0300

    add some

--- server/models/models.js
+++ server/models/models.js
@@ -7,3 +7,11 @@ const User = sequelize.define('user', {
     password: { type: DataTypes.STRING },
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 })
+
+const Bascket = sequelize.define('bascket', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },    
+})
+
+const BascketDevice = sequelize.define('bascket_device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },    
+})
