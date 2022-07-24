92fe4d43fb3dbfdc5b102b97404ebd1cfafbbad2
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-07-25 00:55:25 +0300

    complete models.js

--- server/models/models.js
+++ server/models/models.js
@@ -15,3 +15,75 @@ const Bascket = sequelize.define('bascket', {
 const BascketDevice = sequelize.define('bascket_device', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },    
 })
+
+const Device = sequelize.define('device', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+    price: {type: DataTypes.INTEGER, allowNull: false },
+    rating: {type: DataTypes.INTEGER, defaultValue: 0 },
+    img: {type: DataTypes.STRING, allowNull: false },
+})
+
+const Type = sequelize.define('type', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },  
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+})
+
+const Brand = sequelize.define('brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },  
+    name: { type: DataTypes.STRING, unique: true, allowNull: false },
+})
+
+const Rating = sequelize.define('rating', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },  
+    rate: { type: DataTypes.INTEGER, allowNull: false },
+})
+
+const DeviceInfo = sequelize.define('device_info', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },  
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+})
+
+const TypeBrand = sequelize.define('type_brand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },      
+})
+
+User.hasOne(Bascket)
+Bascket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Bascket.hasMany(BascketDevice)
+BascketDevice.belongsTo(Bascket)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BascketDevice)
+BascketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type, {through: TypeBrand})
+
+module.exports = {
+    User,
+    Bascket,
+    BascketDevice,
+    Device,
+    Type,
+    Brand,
+    Rating,
+    TypeBrand,
+    DeviceInfo        
+}
